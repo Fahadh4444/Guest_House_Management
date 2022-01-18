748a66e0493fdd2f0b7221503c4f3246af552361
Fahadh Kasala <mdfahadh2000@gmail.com>
2022-01-18 21:50:51 +0530

    Modified handler.js

--- Serverless/Backend/handler.js
+++ Serverless/Backend/handler.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const app = require('./app');
+const app = require('./serverlessApp');
 const serverless = require('serverless-http')
-module.exports.hello = serverless(app);
-
-// module.exports.hello = (req, res) => {
-//     return res.send("HI");
-// }
+module.exports.apiapp = serverless(app);
