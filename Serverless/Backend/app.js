6549b186161e1e4371f1c01c95450872d3ecfc6a
Fahadh Kasala <mdfahadh2000@gmail.com>
2022-01-19 21:55:19 +0530

    Uploaded app.js file which is used for local port server hosting,

---
+++ Serverless/Backend/app.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+
+//* IMPORT ROUTES
+const authRoutes = require("./routes/authRoutes");
+
+
+
+//* DB CONNECTION
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+});
+
+
+
+//* MIDDLEWARES
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+
+//* MY ROUTES
+app.use("/api",authRoutes);
+
+
+
+//* PORT
+const port = process.env.PORT || 7000;
+//* STARTING SERVER
+app.listen(port, () => {
+    console.log(`App is running at ${port}`);
+});
